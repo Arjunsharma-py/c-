17f57a41da2d4a60544ef9cdc882e14fb622ebdf
Arjunsharma-py <arjunsharma5330@gamil.com>
2021-11-22 20:48:56 +0530

    nothing

--- test101/test29.cpp
+++ test101/test29.cpp
@@ -1,2 +1,24 @@
 // this is just a test and i don't want to a proper code
 
+#include<iostream>
+#include<string.h>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+    string m;
+    fstream file;
+    cout<<"Enter the text: ";
+    cin>>m;
+    file.open("file_test.txt");
+    if(file.is_open())
+    {
+        file << m;
+        file.close();
+    }
+    else
+        cout<<"file not found";
+    return 0;  
+}
